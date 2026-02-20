9e705b0080c511f285ea8b337e0ce0a661ee8287
frizehero <bag4sk4r4@gmail.com>
2026-02-20 21:12:13 +0700

    mod: update ERROR midtrans

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
-
+if (process.env.NODE_ENV !== 'production') {
+ require('dotenv').config();
+}
 const express = require("express");
 const app = express();
 const cors = require('cors');
